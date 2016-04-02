0cd8d6287c451f2cc4702eb9417ebf4623dbaea6
Xavier Jodoin <xavier@jodoin.me>
2016-04-02 11:54:19 -0400

    new trader profit calculator

--- plugins/trader.js
+++ plugins/trader.js
@@ -4,76 +4,37 @@ var winston = require('winston');
 var config = require('../config/prod.json');
 var adviceEventEmiter = require('../adviceEventEmiter');
 
+var wallet = require('../wallet');
 var fee = 0.0025;
-var lastBuyPrice = config.lastBuyPrice;
-var lastPosition = config.lastPosition;
 
 var startTrading = function() {
-
   adviceEventEmiter.on('advice', function(advice) {
     winston.info('New advice ' + JSON.stringify(advice));
     var lastAvgPrice = advice.lastAvgPrice;
 
-    if (lastBuyPrice) {
-      var profit;
-      if (advice.type === 'buy') {
-        profit = (lastBuyPrice - lastAvgPrice) / lastAvgPrice;
-      } else if (advice.type === 'sell') {
-        profit = (lastAvgPrice - lastBuyPrice) / lastBuyPrice;
+    wallet.loadWallet(function(err, wallet) {
+      winston.info('Current wallet : ' + wallet);
+      if (advice.type === 'sell' && wallet.eth > 0.0001) {
+        var totalBtc = (wallet.eth * lastAvgPrice) * (1 - fee);
+        winston.info('Total btc without fee ' + totalBtc);
+        var profitBtc = (totalBtc - wallet.ethBtcCost) / wallet.ethBtcCost;
+        winston.info('Profit ' + (profitBtc * 100) + '%');
+
+        if (profitBtc > 0) {
+          winston.info('Sell now !!');
+        }
+
+      } else if (advice.type === 'buy' && wallet.btc > 0.0001) {
+        var totalEth = (wallet.btc / lastAvgPrice) * (1 - fee);
+        winston.info('Total eth without fee ' + totalEth);
+        var profitEth = (totalEth - wallet.btcEthCost) / wallet.btcEthCost;
+        winston.info('Profit ' + (profitEth * 100) + '%');
+
+        if (profitEth > 0) {
+          winston.info('Buy now !!');
+        }
       }
-
-      winston.info('Profit : ' + profit * 100 + ' %');
-      if (profit < fee) {
-        winston.info('Not enought profit to cover fee ' + fee * 100 + '% !!');
-        return;
-      }
-    }
-
-    //TODO should cancel existing sell order
-
-
-    plnx.returnAvailableAccountBalances({
-      key: config.key,
-      secret: config.secret
-    }, function(err, data) {
-
-      if(err) {
-        winston.error(err);
-        return;
-      }
-
-      var wallet = data.exchange;
-      winston.info('Current trading wallet : ' + JSON.stringify(wallet));
-
-      var transactionConfig = {
-        key: config.key,
-        secret: config.secret
-      };
-      transactionConfig.currencyPair = 'BTC_ETH';
-      transactionConfig.rate = lastAvgPrice;
-
-      // buy: { currencyPair, rate, amount, fillOrKill?, immediateOrCancel? }
-      if (advice.type === 'buy' && wallet.BTC > 0.0001 && lastPosition === 'sell') {
-        lastBuyPrice = lastAvgPrice;
-        transactionConfig.amount = wallet.BTC / lastAvgPrice;
-        winston.info('Buy order : ' + JSON.stringify(transactionConfig));
-        lastPosition = 'buy';
-        plnx.buy(transactionConfig, function(err, data) {
-          winston.info(err, data);
-        });
-      } else if (advice.type === 'sell' && wallet.ETH > 0.0001 && lastPosition === 'buy') {
-        lastBuyPrice = lastAvgPrice;
-        transactionConfig.amount = wallet.ETH;
-        winston.info('Sell order : ' + JSON.stringify(transactionConfig));
-        lastPosition = 'sell';
-        plnx.sell(transactionConfig, function(err, data) {
-          winston.info(err, data);
-        });
-      }
-
     });
-
-
 
   });
 };
